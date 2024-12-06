0a9699bf87c3ce847bbfdc028a698c9f491370b0
Smarika Shrestha <shrestha.smarika04@gmail.com>
2024-12-06 12:12:19 +0545

    lab1

---
+++ Smarika/lab2/lab2.py
@@ -0,0 +1,20 @@
+x1=int(input('Enter the value of x0'))
+y1=int(input('Enter the value of y0'))
+x2=int(input('Enter the value of x1'))
+y2=int(input('Enter the value of y1'))
+del_x=x2-x1
+del_y=y2-y1
+if(x2>x1)
+{
+    sx=1
+}
+else{
+    sx=-1
+}
+if(y2>y1){
+    sy=1
+}
+else{
+    sy=-1
+}
+x=x1
