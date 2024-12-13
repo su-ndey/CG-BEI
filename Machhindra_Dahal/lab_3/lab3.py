43915b8b13eb19030d295e12be5ac776208903b0
Acer_Machhindra <machhindradahal59@gmail.com>
2024-12-13 12:10:47 +0545

    3 fixed

--- Machhindra_Dahal/lab_3/lab3.py
+++ Machhindra_Dahal/lab_3/lab3.py
@@ -1,22 +1,25 @@
 import matplotlib.pyplot as plt
 
-def circle(r):
-    x=int(0)
-    y=int(r)
-    p=(1-r)
+def circle(r, x1, y1):
+    x = 0
+    y = r
+    p = 1 - r
     xes = []
     yes = []
-    while(x<y):
-        xes.extend([x,x,y,-y,-x,-x,-y,y])
-        yes.extend([y,-y,-x,-x,-y,y,x,x])
-        x= x + 1
-        if p<0 :
-            p = p + 2*x + 1
-        if p>=0 :
+    while x <= y:
+        xes.extend([x + x1, x + x1, y + x1, -y + x1, -x + x1, -x + x1, -y + x1, y + x1])
+        yes.extend([y + y1, -y + y1, -x + y1, -x + y1, -y + y1, y + y1, x + y1, x + y1])
+        x = x + 1
+        if p < 0:
+            p = p + 2 * x + 1
+        else:
             y = y - 1
-            p = p + 2*(x-y) + 1
+            p = p + 2 * (x - y) + 1
     plt.grid()
     plt.scatter(xes, yes, marker='o')
     plt.show()
-r=int(input("enter radius"))
-circle(r)
+
+r = int(input("Enter radius: "))
+x1 = int(input("Enter x coordinate of center: "))
+y1 = int(input("Enter y coordinate of center: "))
+circle(r, x1, y1)
